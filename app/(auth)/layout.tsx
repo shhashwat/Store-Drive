8cd9f5f76c58045243f8de0a6ef3b6cd26f03c5c
Shashwat <sshashwatssingh@gmail.com>
2024-11-15 17:05:43 +0530

    15thNov

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Layout = () => {
+const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>Layout</div>
+    <div className='flex min-h-screen' >
+        <section className='bg-brand p-10'>
+            <div>
+                <Image src="/favicon.ico"
+                alt="logo"
+                width={16}
+                height={16}
+                className='h-auto'/>
+                <div className='space-y-5 text-white'>
+                    <h1 className='h1' >
+                        Organise your files efficiently
+                    </h1>
+                    <p className='body-1'>
+                        Store and manage all your files in one place.
+                    </p>
+                </div>
+            </div>
+        </section>
+        {children}
+    </div>
   )
 }
 
