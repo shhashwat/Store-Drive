0c780fa2f9966aaf431400bfb5e91ec90459dbf2
Shashwat <sshashwatssingh@gmail.com>
2024-12-12 05:25:07 +0530

    Implemented Sort By functionaility

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react'
 import {
   Select,
@@ -6,11 +8,33 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { usePathname, useRouter } from 'next/navigation';
+import { sortTypes } from '@/constants';
 
 
 const Sort = () => {
+  const path = usePathname();
+  const router = useRouter();
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  }
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className='sort-select-content' >
+        {sortTypes.map((sort) => (
+          <SelectItem
+          key={sort.label}
+          className='shad-select-item'
+          value={sort.value}>
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+
   )
 }
 
